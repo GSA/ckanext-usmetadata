7e6338c30df3463b5acf63ebe396bffc4c77b120
Marina Martin <git@marinamartin.com>
2013-05-19 05:19:11 -0400

    Updated access level facet

--- plugin.py
+++ plugin.py
@@ -44,7 +44,7 @@ class IFacetPlugin(plugins.SingletonPlugin):
 
     def dataset_facets(self, facets_dict, dataset_type):
 
-        facets_dict = {'extras_accessLevels': 'Access Level', 'tags': 'Keywords', 'organization': 'Organizations', 'res_format': ('File Format')}
+        facets_dict = {'extras_accessLevel': 'Access Level', 'tags': 'Keywords', 'organization': 'Organizations', 'res_format': ('File Format')}
 
         return facets_dict
 
