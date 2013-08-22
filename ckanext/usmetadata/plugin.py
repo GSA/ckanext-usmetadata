332f30c5f02fae86af2e4cbb3c97c9b2e3f9d8f3
dwcaraway <dave@fogmine.com>
2013-08-22 03:48:39 -0400

    - added mock as a test requirements and added requirements.txt for it - switched setup.py to point to common metadata form - messed around with tests some more

--- ckanext/usmetadata/plugin.py
+++ ckanext/usmetadata/plugin.py
@@ -1,32 +1,128 @@
-import logging
+from logging import getLogger
 
-from ckan.plugins import implements, SingletonPlugin, toolkit, IConfigurer, IDatasetForm, ITemplateHelpers, IFacets
+from ckan.plugins import implements, SingletonPlugin, toolkit, IConfigurer, ITemplateHelpers
 
-# class IFacetPlugin(SingletonPlugin):
-#     implements(IFacets, inherit=True)
-#
-#     def dataset_facets(self, facets_dict, dataset_type):
-#         facets_dict = {'extras_accessLevel': 'Access Level', 'tags': 'Keywords', 'organization': 'Organizations',
-#                        'res_format': ('File Format')}
-#
-#         return facets_dict
+log = getLogger(__name__)
 
-#TODO is this class needed at all?
-class USMetadataPlugin(SingletonPlugin):
+def _create_access_level_vocabulary():
+    '''Create accessLevels vocab and tags, if they don't exist already.'''
+    user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
 
+    log.debug("Creating vocab 'public_access_levels'")
+    data = {'name': 'public_access_level'}
+    vocab = toolkit.get_action('vocabulary_create')(context, data)
+    log.debug('Vocab created: %s' % vocab)
+    for tag in (u'public', u'restricted', u'private'):
+        log.debug(
+            "Adding tag {0} to vocab 'public_access_levels'".format(tag))
+        data = {'name': tag, 'vocabulary_id': vocab['id']}
+        toolkit.get_action('tag_create')(context, data)
+    return vocab
+
+def get_access_levels():
+    log.debug('get_access_levels() called')
+
+    user = toolkit.get_action('get_site_user')({'ignore_auth': True}, {})
+    context = {'user': user['name']}
+
+    vocab = None
+    try:
+        data = {'id': 'public_access_level'} #we can use the id or name for id param
+        vocab = toolkit.get_action('vocabulary_show')(context, data)
+    except:
+        log.debug("vocabulary_show failed, meaning the vocabulary for access level doesn't exist")
+        vocab = _create_access_level_vocabulary()
+
+    access_levels = [x['display_name'] for x in vocab['tags']]
+    log.debug("vocab tags: %s" % access_levels)
+
+    return access_levels
+
+class CommonCoreMetadataForm(SingletonPlugin, toolkit.DefaultDatasetForm):
+    '''This plugin adds fields for the metadata (known as the Common Core) defined at
+    https://github.com/project-open-data/project-open-data.github.io/blob/master/schema.md
+    '''
+
+    implements(ITemplateHelpers, inherit=False)
     implements(IConfigurer, inherit=False)
+
+    def is_fallback(self):
+        # Return True so that we use the extension's dataset form instead of CKAN's default for
+        # /dataset/new and /dataset/edit
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        #
+        return ['dataset', 'package']
 
     def update_config(self, config):
         # Instruct CKAN to look in the ```templates``` directory for customized templates and snippets
         toolkit.add_template_directory(config, 'templates')
 
-    # def is_fallback(self):
-    #     # Return True so that we use the extension's dataset form instead of CKAN's default for
-    #     # /dataset/new and /dataset/edit
-    #     return True
-    #
-    # def package_types(self):
-    #     # This plugin doesn't handle any special package types, it just
-    #     # registers itself as the default (above).
-    #     #
-    #     return []
+    def _modify_package_schema(self, schema):
+        log.debug("_modify_package_schema called")
+        schema.update({
+            'public_access_levels': [toolkit.get_validator('not_empty'),
+                              toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'publisher': [toolkit.get_validator('not_empty'),
+                              toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'contact_name': [toolkit.get_validator('not_empty'),
+                              toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'contact_email': [toolkit.get_validator('not_empty'),
+                              toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'system_of_record': [toolkit.get_validator('not_empty'),
+                                 toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'granularity': [toolkit.get_validator('not_empty'),
+                            toolkit.get_converter('convert_to_tags')]
+        })
+
+        schema.update({
+            'data_dictionary': [toolkit.get_validator('not_empty'),
+                               toolkit.get_converter('convert_to_extras')]
+        })
+        return schema
+
+    def create_package_schema(self):
+        log.debug("create_package_schema called")
+        schema = super(CommonCoreMetadataForm, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        log.debug("update_package_schema called")
+        schema = super(CommonCoreMetadataForm, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def show_package_schema(self):
+        log.debug("show_package_schema called")
+        schema = super(CommonCoreMetadataForm, self).show_package_schema()
+
+        # Don't show vocab tags mixed in with normal 'free' tags
+        # (e.g. on dataset pages, or on the search page)
+        schema['tags']['__extras'].append(toolkit.get_converter('free_tags_only'))
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    #Method below allows functions and other methods to be called from the Jinja template using the h variable
+    def get_helpers(self):
+        log.debug('get_helpers() called, getting the access levels')
+        return {'public_access_levels': get_access_levels}
--- ckanext/usmetadata/tests/test_api.py
+++ ckanext/usmetadata/tests/test_api.py
@@ -1,20 +1,62 @@
 import unittest
-from ckanext.usmetadata.plugin import USMetadataPlugin
-
+from mock import MagicMock, patch
+from nose.tools.nontrivial import with_setup
+import ckanext.usmetadata.plugin as plugin
+from ckan.plugins import toolkit
 
 class MetadataPluginTest(unittest.TestCase):
+    original_get_action = None
+    original_plugin = None
 
-    meta = USMetadataPlugin()
+    def test_get_access_levels(self):
+        assert plugin.get_access_levels() != None
 
-    def test_foo(self):
-        assert True == True
+    # def setup_func(self):
+    #     self.original_get_action = toolkit.get_action
+    #     self.original_plugin = self.plugin
+    #
+    # def teardown_func(self):
+    #     toolkit.get_action = self.original_get_action
+    #     self.plugin = self.original_plugin
+    #
+    # @with_setup(setup_func, teardown_func)
+    # def test_get_access_levels(self):
+    #     '''Test of get_access_levels() function when no exception is raised'''
+    #     toolkit.get_action = MagicMock(name='get_action')
+    #     toolkit.get_action.return_value = MagicMock(); #Don't care what gets called with the action
+    #
+    #     #Invoke unit under test
+    #     plugin.get_access_levels()
+    #
+    #     #Verify results
+    #     toolkit.get_action.assert_called_with('tag_list')
+    #     toolkit.get_action.return_value.assert_called_with(data_dict={'vocabulary_id': 'access_levels'})
 
-    def test_fail(self):
-        assert False == True
+    #TODO probably remove all of the below
+    # @with_setup(setup_func, teardown_func)
+    # def test_get_access_levels_exception(self):
+    #     '''Test of get_access_levels() function, ensure create_access_levels called when access_levels vocabulary
+    #     doesn't exist'''
+    #     toolkit.get_action = MagicMock(name='get_action', side_effect=toolkit.ObjectNotFound("Whoops, couldn't find "
+    #                                                                                          "the access_levels "
+    #                                                                                          "vocabulary. Bet ya didn't"
+    #                                                                                          " create it"))
+    #     plugin.create_access_levels = MagicMock(name='create_access_levels')
+    #     plugin.get_user_context = MagicMock(name='get_user_context', return_value={'user':'aardvark'})
+    #
+    #     #Invoke unit under test
+    #     plugin.get_access_levels()
+    #
+    #     #Verify results
+    #     toolkit.get_action.assert_called_once_with('tag_list')
+    #     #Verify call to create the access_levels vocabulary
+    #     plugin.create_access_levels.assert_called_once_with(plugin.get_user_context.return_value)
 
-    def test_setup(self):
-        assert self.meta == None
+    # #TODO Test addition of tag vocabularies
+    # def test_get_user_context(self):
+    #     context = plugin.get_user_context();
+    #     assert context != None
+    #     print context
 
-    #TODO Test addition of tag vocabularies via Action API
     #TODO test customization of the form
     #TODO test addition to templates
