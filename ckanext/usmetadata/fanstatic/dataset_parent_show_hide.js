ae1937d396b23847c8bf7b2c266344f2c12b612b
ykhadilkar <ykhadilkar@reisys.com>
2014-11-14 13:44:17 -0500

    Issue #153 - Updates for Schema 1.1 - remove current dataset from parents list + remove current parent dataset field if dataset changed to be Parent.

--- ckanext/usmetadata/fanstatic/dataset_parent_show_hide.js
+++ ckanext/usmetadata/fanstatic/dataset_parent_show_hide.js
@@ -2,14 +2,17 @@
  * Created by ykhadilkar on 11/11/14.
  */
 $(document).ready(function(){
+    $()
     $("#field-is_parent").change(function(){
-        $( "select option:selected").each(function(){
-            if($(this).attr("value")=="false"){
-                $(".control-group-dataset-parent").show();
-            }
-            if($(this).attr("value")=="true"){
-                $(".control-group-dataset-parent").hide();
-            }
-        });
+        $val = $("#field-is_parent").val();
+        if($val=="false"){
+            $(".control-group-dataset-parent").show();
+            //remove current dataset from parent dataset list
+            $("#field-parent_dataset option[value='"+$('[name="pkg_name"]').val()+"']").remove();
+        }
+        if($val=="true"){
+            $(".control-group-dataset-parent").hide();
+            $("#field-parent_dataset").val("");
+        }
     }).change();
 });
