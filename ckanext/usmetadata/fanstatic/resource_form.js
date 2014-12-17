62ee6b78933c8e4e1bd944af314f13272ac6c014
Alex Perfilov <alexandr.perfilov@reisystems.com>
2014-12-17 18:10:45 -0500

    resource missing

--- ckanext/usmetadata/fanstatic/resource_form.js
+++ ckanext/usmetadata/fanstatic/resource_form.js
@@ -2,25 +2,31 @@ $(document).ready(function(){
     $('input[name="resource_type"]').change(
         function(){resource_type_change($(this).val());}
     );
-});
 
-window.setInterval(function(){
-  /// call your function here
-    $('.resource-upload .btn').eq(2).remove();
-    $('.resource-url .btn-remove-url').remove();
-    resource_type_change($('input[name="resource_type"]:checked').val());
-}, 500);
+    $('form.dataset-resource-form').submit(function(){
+        if (!$('input[name="url"]').val()) {
+            $('input[name="resource_type"]').prop("checked", false);
+        }
+    })
+
+    window.setInterval(function(){
+        $('.resource-upload .btn').eq(2).remove();
+        $('.resource-url .btn-remove-url').remove();
+        resource_type_change($('input[name="resource_type"]:checked').val());
+    }, 500);
+});
 
 function resource_type_change(val)
 {
+    if (!val) return;
+    $('.image-upload').show();
     switch(val) {
         case 'upload':
             $('.resource-upload').show();
             if ($('input[name="url"]').val()) {
                 $('.resource-upload .btn').eq(1).show();
-            } else {
-                $('.resource-upload .btn').eq(0).show();
             }
+            $('.resource-upload .btn').eq(0).show();
             $('#field-image-upload').show();
             $('.resource-url').hide();
             break;
