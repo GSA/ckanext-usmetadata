e5659571645a06a2b143d51145aede97b66c466c
Alex Perfilov <alexandr.perfilov@reisystems.com>
2014-12-17 17:02:14 -0500

    js fix

--- ckanext/usmetadata/fanstatic/resource_form.js
+++ ckanext/usmetadata/fanstatic/resource_form.js
@@ -8,6 +8,7 @@ window.setInterval(function(){
   /// call your function here
     $('.resource-upload .btn').eq(2).remove();
     $('.resource-url .btn-remove-url').remove();
+    resource_type_change($('input[name="resource_type"]:checked').val());
 }, 500);
 
 function resource_type_change(val)
@@ -15,8 +16,12 @@ function resource_type_change(val)
     switch(val) {
         case 'upload':
             $('.resource-upload').show();
+            if ($('input[name="url"]').val()) {
+                $('.resource-upload .btn').eq(1).show();
+            } else {
+                $('.resource-upload .btn').eq(0).show();
+            }
             $('#field-image-upload').show();
-            $('.resource-upload .btn').first().show();
             $('.resource-url').hide();
             break;
         default:
