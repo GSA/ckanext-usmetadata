07d2464ac21a78379ac863e037f8338ac3fe149e
ykhadilkar <ykhadilkar@reisys.com>
2015-02-11 16:10:23 -0500

    Fixing parent dataset dropdown + show facets on organization page

--- ckanext/usmetadata/db_utils.py
+++ ckanext/usmetadata/db_utils.py
@@ -7,13 +7,20 @@ import ckan.model as model
 
 cached_tables = {}
 
-def get_parent_organizations(limit=20):
+def get_parent_organizations(c):
+
+    userGroupsIds = c.userobj.get_group_ids()
+    ids = []
+    for id in userGroupsIds:
+        ids.append(id.encode('ascii','ignore'))
     items = {}
     connection = model.Session.connection()
-    res = connection.execute(
-        text("""select package_id, title
-                from package_extra, package
-                where package_extra.key = 'is_parent' and package_extra.value = 'true' and package.id = package_extra.package_id""")).fetchmany(limit)
+    query = "select package_id, title from package_extra , package " \
+            "where package_extra.key = 'is_parent' and package_extra.value = 'true' " \
+            "and package.id = package_extra.package_id and package.state = 'active' " \
+            "and package.owner_org in "+str(tuple(ids))
+
+    res = connection.execute(query).fetchall()
     for result in res:
         items[result._row[0]] = result._row[1]
 
