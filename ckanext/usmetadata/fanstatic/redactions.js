11968ce66da5987b839a1133d756d317d9ef4989
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-10-19 17:50:55 -0400

    ie8 js fix

--- ckanext/usmetadata/fanstatic/redactions.js
+++ ckanext/usmetadata/fanstatic/redactions.js
@@ -152,8 +152,50 @@ var RedactionControl = new function () {
             return;
         }
 
-        var selectionStart = input[0].selectionStart;
-        var selectionEnd = input[0].selectionEnd;
+        var start = 0, end = 0, normalizedValue, range,
+            textInputRange, len, endRange;
+
+        var el = input[0];
+
+        if (typeof el.selectionStart == "number" && typeof el.selectionEnd == "number") {
+            start = el.selectionStart;
+            end = el.selectionEnd;
+        } else {
+            range = document.selection.createRange();
+
+            if (range && range.parentElement() == el) {
+                len = el.value.length;
+                normalizedValue = el.value.replace(/\r\n/g, "\n");
+
+                // Create a working TextRange that lives only in the input
+                textInputRange = el.createTextRange();
+                textInputRange.moveToBookmark(range.getBookmark());
+
+                // Check if the start and end of the selection are at the very end
+                // of the input, since moveStart/moveEnd doesn't return what we want
+                // in those cases
+                endRange = el.createTextRange();
+                endRange.collapse(false);
+
+                if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
+                    start = end = len;
+                } else {
+                    start = -textInputRange.moveStart("character", -len);
+                    start += normalizedValue.slice(0, start).split("\n").length - 1;
+
+                    if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
+                        end = len;
+                    } else {
+                        end = -textInputRange.moveEnd("character", -len);
+                        end += normalizedValue.slice(0, end).split("\n").length - 1;
+                    }
+                }
+            }
+        }
+
+        var selectionStart = start;
+        var selectionEnd = end;
+
         if (!selectionEnd || selectionStart == selectionEnd) {
             return;
         }
@@ -280,7 +322,7 @@ $(document).ready(function () {
         $('.redacted-md')
             .each(function () {
                 if ($(this).siblings('img.redacted-icon').length) {
-                    $(this).html($(this).html().replace(/\~\~(.*?)\~\~/g,'<del>$1</del>'));
+                    $(this).html($(this).html().replace(/\~\~(.*?)\~\~/g, '<del>$1</del>'));
                 } else {
                     $(this).html($(this).html().replace(/\~/g, ''));
                 }
