a927852d4d7fc7d573fda39ad9e8b6d9cba78faf
Fuhu Xia <fxia@reisystems.com>
2021-01-15 12:53:56 -0500

    flake8

--- ckanext/usmetadata/tests/test_usmetadata.py
+++ ckanext/usmetadata/tests/test_usmetadata.py
@@ -9,6 +9,7 @@ import ckan.model as model
 from tests.legacy import call_action_api
 import ckan.plugins as plugins
 
+
 class TestUsmetadataPlugin(object):
     '''Tests for the usmetadata.plugin module.
 
@@ -34,23 +35,24 @@ class TestUsmetadataPlugin(object):
 
         self.org_dict = call_action_api(self.app, 'organization_create', apikey=self.sysadmin.get('apikey'), name='my_org_000')
 
-        self.package_dict = call_action_api(self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package_000',
-                                             title='my package',
-                                             notes='my package notes',
-                                             tag_string='my_package',
-                                             modified='2014-04-04',
-                                             publisher='GSA',
-                                             contact_name='john doe',
-                                             contact_email='john.doe@gsa.com',
-                                             unique_id='000',
-                                             public_access_level='public',
-                                             bureau_code='001:40',
-                                             program_code='015:010',
-                                             access_level_comment='Access level commemnt',
-                                             parent_dataset = 'true',
-                                             ower_org = self.org_dict['id']
-                                             )
+        self.package_dict = call_action_api(
+            self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
+            name='my_package_000',
+            title='my package',
+            notes='my package notes',
+            tag_string='my_package',
+            modified='2014-04-04',
+            publisher='GSA',
+            contact_name='john doe',
+            contact_email='john.doe@gsa.com',
+            unique_id='000',
+            public_access_level='public',
+            bureau_code='001:40',
+            program_code='015:010',
+            access_level_comment='Access level commemnt',
+            parent_dataset='true',
+            ower_org=self.org_dict['id']
+        )
 
     def teardown(self):
         '''Nose runs this method after each test method in our test class.'''
@@ -69,123 +71,130 @@ class TestUsmetadataPlugin(object):
         # tests that run after ours.
         plugins.unload('usmetadata')
 
-    #test is dataset is getting created successfully
+    # test is dataset is getting created successfully
     def test_package_creation(self):
-        package_dict = call_action_api(self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package',
-                                             title='my package',
-                                             notes='my package notes',
-                                             tag_string='my_package',
-                                             modified='2014-04-04',
-                                             publisher='GSA',
-                                             contact_name='john doe',
-                                             contact_email='john.doe@gsa.com',
-                                             unique_id='001',
-                                             public_access_level='public',
-                                             bureau_code='001:40',
-                                             program_code='015:010',
-                                             access_level_comment='Access level commemnt',
-                                             parent_dataset = 'true'
-                                             )
+        package_dict = call_action_api(
+            self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
+            name='my_package',
+            title='my package',
+            notes='my package notes',
+            tag_string='my_package',
+            modified='2014-04-04',
+            publisher='GSA',
+            contact_name='john doe',
+            contact_email='john.doe@gsa.com',
+            unique_id='001',
+            public_access_level='public',
+            bureau_code='001:40',
+            program_code='015:010',
+            access_level_comment='Access level commemnt',
+            parent_dataset='true'
+        )
         assert package_dict['name'] == 'my_package'
-
 
     # test resource creation
     def test_resource_create(self):
         print "first"
-        package_dict = call_action_api(self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package',
-                                             title='my package',
-                                             notes='my package notes',
-                                             tag_string='my_package',
-                                             modified='2014-04-04',
-                                             publisher='GSA',
-                                             publisher_1='OCSIT',
-                                             contact_name='john doe',
-                                             contact_email='john.doe@gsa.com',
-                                             unique_id='001',
-                                             public_access_level='public',
-                                             bureau_code='001:40',
-                                             program_code='015:010',
-                                             access_level_comment='Access level commemnt',
-                                             license_id='http://creativecommons.org/publicdomain/zero/1.0/',
-                                             license_new='http://creativecommons.org/publicdomain/zero/1.0/',
-                                             spatial='Lincoln, Nebraska',
-                                             temporal='2000-01-15T00:45:00Z/2010-01-15T00:06:00Z',
-                                             category=["vegetables","produce"],
-                                             data_dictionary='www.google.com',
-                                             data_dictionary_type='tex/csv',
-                                             data_quality='true',
-                                             publishing_status='open',
-                                             accrual_periodicity='annual',
-                                             conforms_to='www.google.com',
-                                             homepage_url='www.google.com',
-                                             language='us-EN',
-                                             primary_it_investment_uii='021-123456789',
-                                             related_documents='www.google.com',
-                                             release_date='2014-01-02',
-                                             system_of_records='www.google.com',
-                                             is_parent='true',
-                                             accessURL='www.google.com',
-                                             webService='www.gooogle.com',
-                                             format='text/csv',
-                                             formatReadable='text/csv',
-                                             resources=[
-                                                 {
-                                                    'name':'my_resource',
-                                                    'url':'www.google.com',
-                                                    'description':'description'},
-                                                 { 'name':'my_resource_1',
-                                                    'url':'www.google.com',
-                                                    'description':'description_2'},
-                                                ]
-                                             )
+        package_dict = call_action_api(
+            self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
+            name='my_package',
+            title='my package',
+            notes='my package notes',
+            tag_string='my_package',
+            modified='2014-04-04',
+            publisher='GSA',
+            publisher_1='OCSIT',
+            contact_name='john doe',
+            contact_email='john.doe@gsa.com',
+            unique_id='001',
+            public_access_level='public',
+            bureau_code='001:40',
+            program_code='015:010',
+            access_level_comment='Access level commemnt',
+            license_id='http://creativecommons.org/publicdomain/zero/1.0/',
+            license_new='http://creativecommons.org/publicdomain/zero/1.0/',
+            spatial='Lincoln, Nebraska',
+            temporal='2000-01-15T00:45:00Z/2010-01-15T00:06:00Z',
+            category=["vegetables", "produce"],
+            data_dictionary='www.google.com',
+            data_dictionary_type='tex/csv',
+            data_quality='true',
+            publishing_status='open',
+            accrual_periodicity='annual',
+            conforms_to='www.google.com',
+            homepage_url='www.google.com',
+            language='us-EN',
+            primary_it_investment_uii='021-123456789',
+            related_documents='www.google.com',
+            release_date='2014-01-02',
+            system_of_records='www.google.com',
+            is_parent='true',
+            accessURL='www.google.com',
+            webService='www.gooogle.com',
+            format='text/csv',
+            formatReadable='text/csv',
+            resources=[
+                {
+                    'name': 'my_resource',
+                    'url': 'www.google.com',
+                    'description': 'description'
+                },
+                {
+                    'name': 'my_resource_1',
+                    'url': 'www.google.com',
+                    'description': 'description_2'
+                },
+            ]
+        )
 
         assert package_dict['name'] == 'my_package'
         assert package_dict['resources'][0]['name'] == 'my_resource'
 
         print "second"
-        resource_dict = call_action_api(self.app, 'resource_create', apikey=self.sysadmin.get('apikey'),
-                                              package_id = package_dict['id'],
-                                              name='my_resource_2',
-                                              url='www.google.com',
-                                              description='description_3'
-                                             )
+        resource_dict = call_action_api(
+            self.app, 'resource_create', apikey=self.sysadmin.get('apikey'),
+            package_id=package_dict['id'],
+            name='my_resource_2',
+            url='www.google.com',
+            description='description_3'
+        )
         assert resource_dict['name'] == 'my_resource_2'
 
-    #test package update
+    # test package update
     def test_package_update(self):
-        package_dict = call_action_api(self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package',
-                                             title='my package',
-                                             notes='my package notes',
-                                             tag_string='my_package',
-                                             modified='2014-04-04',
-                                             publisher='GSA',
-                                             contact_name='john doe',
-                                             contact_email='john.doe@gsa.com',
-                                             unique_id='001',
-                                             public_access_level='public',
-                                             bureau_code='001:40',
-                                             program_code='015:010',
-                                             access_level_comment='Access level commemnt'
-                                             )
+        package_dict = call_action_api(
+            self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
+            name='my_package',
+            title='my package',
+            notes='my package notes',
+            tag_string='my_package',
+            modified='2014-04-04',
+            publisher='GSA',
+            contact_name='john doe',
+            contact_email='john.doe@gsa.com',
+            unique_id='001',
+            public_access_level='public',
+            bureau_code='001:40',
+            program_code='015:010',
+            access_level_comment='Access level commemnt'
+        )
         assert package_dict['name'] == 'my_package'
-        package_dict_update = call_action_api(self.app, 'package_update', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package',
-                                             title='my package update',
-                                             notes='my package notes update',
-                                             tag_string='my_package',
-                                             modified='2014-04-05',
-                                             publisher='GSA',
-                                             contact_name='john doe jr',
-                                             contact_email='john.doe1@gsa.com',
-                                             unique_id='002',
-                                             public_access_level='public',
-                                             bureau_code='001:41',
-                                             program_code='015:011',
-                                             access_level_comment='Access level commemnt update'
-                                             )
+        package_dict_update = call_action_api(
+            self.app, 'package_update', apikey=self.sysadmin.get('apikey'),
+            name='my_package',
+            title='my package update',
+            notes='my package notes update',
+            tag_string='my_package',
+            modified='2014-04-05',
+            publisher='GSA',
+            contact_name='john doe jr',
+            contact_email='john.doe1@gsa.com',
+            unique_id='002',
+            public_access_level='public',
+            bureau_code='001:41',
+            program_code='015:011',
+            access_level_comment='Access level commemnt update'
+        )
         assert package_dict_update['title'] == 'my package update'
 
         assert package_dict_update['extras'][0]['value'] == 'Access level commemnt update'
@@ -198,28 +207,29 @@ class TestUsmetadataPlugin(object):
         assert package_dict_update['extras'][7]['value'] == 'GSA'
         assert package_dict_update['extras'][8]['value'] == '002'
 
-    #test parent dataset
+    # test parent dataset
     def test_package_parent_dataset(self):
         org_dict = call_action_api(self.app, 'organization_create', apikey=self.sysadmin.get('apikey'),
                                              name='my_org')
 
-        package_dict = call_action_api(self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
-                                             name='my_package',
-                                             title='my package',
-                                             notes='my package notes',
-                                             tag_string='my_package',
-                                             modified='2014-04-04',
-                                             publisher='GSA',
-                                             contact_name='john doe',
-                                             contact_email='john.doe@gsa.com',
-                                             unique_id='001',
-                                             public_access_level='public',
-                                             bureau_code='001:40',
-                                             program_code='015:010',
-                                             access_level_comment='Access level commemnt',
-                                             parent_dataset = 'true',
-                                             ower_org = org_dict['id']
-                                             )
+        package_dict = call_action_api(
+            self.app, 'package_create', apikey=self.sysadmin.get('apikey'),
+            name='my_package',
+            title='my package',
+            notes='my package notes',
+            tag_string='my_package',
+            modified='2014-04-04',
+            publisher='GSA',
+            contact_name='john doe',
+            contact_email='john.doe@gsa.com',
+            unique_id='001',
+            public_access_level='public',
+            bureau_code='001:40',
+            program_code='015:010',
+            access_level_comment='Access level commemnt',
+            parent_dataset='true',
+            ower_org=org_dict['id']
+        )
         assert package_dict['name'] == 'my_package'
 
         title = db_utils.get_organization_title(package_dict['id'])
@@ -244,9 +254,9 @@ class TestUsmetadataPlugin(object):
         items = db_utils.get_parent_organizations(config)
         assert org_dict['id'] not in items
 
-    #TODO:Add assertions for all field validations
+    # TODO:Add assertions for all field validations
     def test_validate_dataset_action(self):
-        url = '/api/2/util/resource/validate_dataset?pkg_name=&owner_org='+ self.org_dict['id'] +'&unique_id=000&rights=&license_url=&temporal=&described_by=&described_by_type=&conforms_to=&landing_page=&language=&investment_uii=&references=&issued=&system_of_records='
+        url = '/api/2/util/resource/validate_dataset?pkg_name=&owner_org=' + self.org_dict['id'] + '&unique_id=000&rights=&license_url=&temporal=&described_by=&described_by_type=&conforms_to=&landing_page=&language=&investment_uii=&references=&issued=&system_of_records='
         res = self.app.get(url)
         assert 'Success' in res
 
--- setup.py
+++ setup.py
@@ -3,24 +3,23 @@ from setuptools import setup, find_packages
 version = '0.2.1'
 
 setup(
-	name='ckanext-usmetadata',
-	version=version,
-	description='US Metadata Plugin',
-	long_description='',
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='Sean Herron, created by Marina Martin',
-	author_email='sean.herron@fda.hhs.gov',
-	url='',
-	license='',
-	packages=find_packages(),
-	namespace_packages=['ckanext', 'ckanext.usmetadata'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[],
-	entry_points=\
-	"""
-        [ckan.plugins]
-	    usmetadata=ckanext.usmetadata.plugin:CommonCoreMetadataFormPlugin
-	""",
+    name='ckanext-usmetadata',
+    version=version,
+    description='US Metadata Plugin',
+    long_description='',
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Sean Herron, created by Marina Martin',
+    author_email='sean.herron@fda.hhs.gov',
+    url='',
+    license='',
+    packages=find_packages(),
+    namespace_packages=['ckanext', 'ckanext.usmetadata'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points="""
+    [ckan.plugins]
+    usmetadata=ckanext.usmetadata.plugin:CommonCoreMetadataFormPlugin
+    """,
 )
