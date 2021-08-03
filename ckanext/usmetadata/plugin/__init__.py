b784834ac2d98d199251b4e4deb606d26cea7915
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-08-03 01:54:02 -0400

    tests: everything passes in 2.9; this issue was only a logging type error

--- ckanext/usmetadata/plugin/__init__.py
+++ ckanext/usmetadata/plugin/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from builtins import str
 import collections
 import re
+import six
 from logging import getLogger
 
 from ckan.common import json
@@ -258,8 +259,12 @@ class CommonCoreMetadataFormPlugin(MixinPlugin, p.SingletonPlugin, p.toolkit.Def
 
             new_dict['extras'] = reduced_extras
         except KeyError as ex:
-            log.debug('''Expected key ['%s'] not found, attempting to move DCAT-US keys to subdictionary''',
-                      ex.message)
+            if six.PY2:
+                log.debug('''Expected key ['%s'] not found, attempting to move DCAT-US keys to subdictionary''',
+                          ex.message)
+            else:
+                log.debug('''Expected key ['%s'] not found, attempting to move DCAT-US keys to subdictionary''',
+                          ex)
             # this can happen when a form fails validation, as all the data will now be as key,
             # value pairs, not under extras, so we'll move them to the expected point again to fill in the values
             # e.g.
