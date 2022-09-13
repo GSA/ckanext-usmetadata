d31d9b345e54783121a83bbf4d4fdf0bd9fad754
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-13 14:10:50 -0400

    fix: filepath

--- ckanext/usmetadata/blueprint.py
+++ ckanext/usmetadata/blueprint.py
@@ -18,7 +18,7 @@ import ckan.lib.plugins
 import ckan.logic as logic
 import ckan.model as model
 from ckan.plugins.toolkit import c, config, request, requires_ckan_version
-from ckanext.usmetadata.plugin import helper as local_helper
+from ckanext.usmetadata import helper as local_helper
 
 
 requires_ckan_version("2.9")
