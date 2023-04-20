4a76eb02d557287aaf8c160ae8c2a41dbd43f7c8
Jin-Sun-tts <jin.sun@gsa.gov>
2023-04-20 13:48:57 -0400

    sqlalchemy deprecated _row from the result
    reference: https://github.com/sqlalchemy/sqlalchemy/blob/rel_1_4_41/lib/sqlalchemy/engine/row.py#L390 and https://github.com/sqlalchemy/sqlalchemy/blob/rel_1_3_5/lib/sqlalchemy/engine/result.py#L59

--- ckanext/usmetadata/db_utils.py
+++ ckanext/usmetadata/db_utils.py
@@ -9,7 +9,7 @@ def get_organization_title(dataset_id):
     query = "select id, title from package where package.id = '" + dataset_id + "'"
     connection = model.Session.connection()
     res = connection.execute(query).fetchone()
-    return res._row[1]
+    return res[1]
 
 
 def get_parent_organizations(c):
