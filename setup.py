ec2c77c57b4842b3789beb7007f3a86dcf7cb7ce
Cody Seibert <cody.seibert@gsa.gov>
2026-06-24 11:49:49 -0400

    revert the quote changes

--- setup.py
+++ setup.py
@@ -5,29 +5,29 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name="ckanext-usmetadata",
-    version="0.3.4",
-    description="US Metadata Plugin for CKAN",
+    name='ckanext-usmetadata',
+    version='0.3.4',
+    description='US Metadata Plugin for CKAN',
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     classifiers=[
-        "Programming Language :: Python :: 3"
+        'Programming Language :: Python :: 3'
     ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords="",
-    author="Data.gov",
-    author_email="datagovhelp@gsa.gov",
-    url="https://github.com/GSA/ckanext-usmetadata",
-    license="Public Domain",
+    keywords='',
+    author='Data.gov',
+    author_email='datagovhelp@gsa.gov',
+    url='https://github.com/GSA/ckanext-usmetadata',
+    license='Public Domain',
     packages=find_packages(),
-    namespace_packages=["ckanext", "ckanext.usmetadata"],
+    namespace_packages=['ckanext', 'ckanext.usmetadata'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[],
-    setup_requires=["wheel"],
+    setup_requires=['wheel'],
     entry_points="""
         [ckan.plugins]
             usmetadata=ckanext.usmetadata.plugin:CommonCoreMetadataFormPlugin
